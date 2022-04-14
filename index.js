b0a10366dfde31e042121160ccb122dba0ccabc8
Jake Hankins <jakehankins@comcast.net>
2022-04-13 21:07:40 -0400

    commit tree 8c808cfa2e472ee390ae6724e59bfa558c8809f5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(friends, type){
+    const cards = [];
+    for (let i = 0; i <3; i++){
+        cards.push(`Thank you, ${friends[i]}, for the wonderful ${type} gift!`);
+    }
+    return cards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(){
+    for (let i =10; i >= 0; i--) {
+        console.log(i)
+    }
+    
+}
+countDown(10)
